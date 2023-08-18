9406ffa774eae6b6943c13fde9f2fb55c230be07
unknown <simonmonaco86@gmail.com>
2023-08-17 21:43:23 -0300

    primera conexion a db

---
+++ ejemplo11/PokemonServer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ejemplo11
+{
+    class PokemonServer
+    {
+        public List<Pokemon> listas()
+        {
+            List<Pokemon> lista = new List<Pokemon>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=POKEDEX_DB; Integrated security = true";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "select Numero, NOmbre, Descripcion from POKEMONS";
+                comando.Connection = conexion;
+
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    Pokemon aux = new Pokemon();
+                    aux.Descripcion = (string)lector["Descripcion"];
+                    aux.Numero = lector.GetInt32(0);
+                    aux.Nombre = (string)lector["Nombre"];
+                    
+
+                    lista.Add(aux);
+                }
+
+                conexion.Close();
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+
+    }
+}
